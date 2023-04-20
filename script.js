943087e21eb03d96bd608a71b56555837c80a399
ShemeerCoder-01 <118423467+ShemeerCoder-01@users.noreply.github.com>
2023-04-20 17:26:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,9 +2,11 @@ const getSumBtn = document.createElement("button");
 getSumBtn.append("Get Total Price");
 document.body.appendChild(getSumBtn);
 
+
 const getSum = () => {
 //Add your code here
-  
+  let arr = Array.from(document.getElementsByClassName("price"));
+	alert(arr.innerText);
 };
 
 getSumBtn.addEventListener("click", getSum);
